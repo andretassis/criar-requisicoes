5195d3427cb77c9ac392415730bd75f81b08a5fa
andretassis <tassisandre@gmail.com>
2023-06-14 13:35:06 -0300

    implementação de função para adicionar videos

--- js/conectarApi.js
+++ js/conectarApi.js
@@ -1,3 +1,4 @@
+// conexao para listar os videos dinamicamente
 async function listaVideos() {
     const conexao = await fetch("http://localhost:3000/videos");
     const conexaoJson = await conexao.json();
@@ -5,8 +6,26 @@ async function listaVideos() {
     return conexaoJson
 }
 
-listaVideos()
+// conexao para adicionar um novo video
+async function criarVideos(titulo, descricao, url, imagem) {
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem,
+        })
+    });
+
+    const conexaoJson = await conexao.json();
+    return conexaoJson
+}
 
 export const conexaoApi = {
-    listaVideos
+    listaVideos,
+    criarVideos
 }
